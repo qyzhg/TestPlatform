f66d41e5483d8c4043a5d4f10c710bacde560f19
zhuangqingyao <qyzhg@163.com>
2020-05-21 09:02:39 +0800

    新建视察功能App

---
+++ Pro_inspection/urls.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# -*- encoding: utf-8 -*-
+
+'''
+@Author  :   Zhuang_Qingyao
+ 
+@Contact :   qyzhg@163.com
+ 
+@File    :   urls.py
+ 
+@Time    :   2020/5/21 9:02 上午
+'''
+
+
